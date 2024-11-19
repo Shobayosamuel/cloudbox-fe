0e986ae4f92ea3c7c3750d20bae303b62f485f96
shobayo samuel <shobayosamuel62@gmail.com>
2024-11-19 22:18:46 +0100

    fix: use client

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useAuth } from '../contexts/AuthContext';
 import FileManager from '../components/FileManager';
 import { ProtectedRoute } from '../components/ProtectedRoute';
